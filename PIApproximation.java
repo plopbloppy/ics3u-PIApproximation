626c86090b00006206180198db10bb6a1c5e5d6a
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-02-26 11:57:06 -0500

    Add files via upload

--- PIApproximation.java
+++ PIApproximation.java
@@ -1,25 +1,24 @@
-
 import java.util.Scanner;
 
 public class PIApproximation {
     public static void main(String[] args) throws Exception {
-        char input = 'y';
-        while (input == 'y') {
-            Scanner keyboard = new Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("Please enter the n value:");
+            int n = keyboard.nextInt();
+            double sum = 0.0;
+            
+            for (int i = 0; i <= n; i++) {
+                sum += Math.pow(-1, i) / (2 * i + 1);  
+            }
 
-            if (input == 'y') {
-                System.out.println("Please enter the n value:");
-                double n = keyboard.nextDouble();
+            System.out.println("The value of pi is: " + 4 * sum + ".");
+            System.out.println("Would you like to try again? [y/n]");
+            
+            if  (Character.toLowerCase(keyboard.next().charAt(0)) == 'n') {
                 keyboard.close();
-                for (int i = 0; i < n + 1; i++) {
-                    double sum = 0;
-                    sum += (Math.pow(-1, i)) / (2 * i + 1);
-                    if (i >= n) {
-                        System.out.println("The value of pi is: " + 4 * sum + ".");
-                        System.out.println("Would you like to try again? [y/n]");
-                        input = Character.toLowerCase(keyboard.next().charAt(0));
-                    }
-                }
+                break;
             }
         }
     }
